40c6d6cb1959b4476ddb1252d2f192dedaa62892
Luana Izy Veras Tavares <100386237+luanaizy@users.noreply.github.com>
2023-03-30 19:15:43 -0300

    functions

--- mem_emu.c
+++ mem_emu.c
@@ -15,15 +15,15 @@ uint8_t mem_emu_read_byte(mem_emu_hw_t *mem, uint8_t addr){
 	
 	uint8_t cmd_read[]= {0x03, addr};
 	
-	HAL_SPI_Transmit(&(mem->hspi), cmd_read, 2, 1000);
-	HAL_SPI_Receive(&(mem->hspi), mem, 1, HAL_MAX_DELAY);
+	HAL_SPI_Transmit(mem->hspi, cmd_read, 2, 1000);
+	HAL_SPI_Receive(mem->hspi, mem, 1, HAL_MAX_DELAY);
 }
 
 void mem_emu_write_byte(mem_emu_hw_t *mem, uint8_t addr, uint8_t data){
 	
 	uint8_t cmd_write[]= {0x02, addr, data};
 	
-	HAL_SPI_Transmit(&(mem->hspi), cmd_read, 3, 1000)
+	HAL_SPI_Transmit(mem->hspi, cmd_read, 3, 1000)
 }
 
 
